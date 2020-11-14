400478ecaedaf97f445df8fec07ff5031c7d9ccb
melonedo <44501064+melonedo@users.noreply.github.com>
2020-11-14 15:45:39 +0800

    补充上讲义代码

--- graia/main.py
+++ graia/main.py
@@ -2,17 +2,16 @@ import asyncio
 
 from graia.application import GraiaMiraiApplication as GraiaApp, Session
 from graia.broadcast import Broadcast
-from graia.broadcast.exceptions import ExecutionStop
-from graia.broadcast.builtin.decoraters import Depend
-from graia.application.entry import Friend, FriendMessage, MessageChain, Group, GroupMessage, Plain, ApplicationShutdowned
+from graia.application.entry import Friend, FriendMessage, MessageChain, Group, GroupMessage, Plain
+from graia.application.entry import ApplicationLaunched, MemberMuteEvent, At, MemberUnmuteEvent, Member
 
 import yaml
 import aiohttp
 import time
-# 包含mirai-api-http的配置文件中的对应部分以及qq号
 
 time.sleep(15)
 
+# 包含mirai-api-http的配置文件中的对应部分以及qq号
 with open('setting.yml') as f:
     setting = yaml.load(f, yaml.BaseLoader)
 loop = asyncio.get_event_loop()
@@ -27,19 +26,20 @@ app = GraiaApp(
     )
 )
 
-
-@bcc.receiver(FriendMessage)
+# 复读
+# @bcc.receiver(FriendMessage)
 async def echoer(app: GraiaApp, message: MessageChain, friend: Friend):
     if message.asDisplay().startswith("复读"):
         await app.sendFriendMessage(friend, message.asSendable())
+echoer = bcc.receiver(FriendMessage)(echoer)  # 如果不用修饰器
+
+TEST_GROUP_LIST = [943603660]
 
 
-WHITELIST = [943603660]
-
-
+# 查电费
 @bcc.receiver(GroupMessage)
 async def electricity(app: GraiaApp, message: MessageChain, group: Group):
-    if not (group.id in WHITELIST and message.asDisplay().startswith("电费")):
+    if not (group.id in TEST_GROUP_LIST and message.asDisplay().startswith("电费")):
         return
 
     async with aiohttp.ClientSession() as s:
@@ -52,5 +52,32 @@ async def electricity(app: GraiaApp, message: MessageChain, group: Group):
     else:
         text = f"查询失败，原因：{json['error']}"
     await app.sendGroupMessage(group, MessageChain.create([Plain(text)]))
+
+
+# 开机事件
+@bcc.receiver(ApplicationLaunched)
+async def hello(app: GraiaApp):
+    for group in TEST_GROUP_LIST:
+        await app.sendGroupMessage(group, MessageChain.fromSerializationString(
+            "hello[mirai:image:{43E03605-6CE5-48D3-2CB5-0A52A9512896}.mirai]"
+        ))
+
+
+# 禁言（直接获取事件）
+@bcc.receiver(MemberMuteEvent)
+async def mute_notice(app: GraiaApp, event: MemberMuteEvent):
+    await app.sendGroupMessage(event.member.group, MessageChain.create([
+        At(event.member.id),
+        Plain("被卷死了")
+    ]))
+
+
+# 解除禁言（利用注释）
+@bcc.receiver(MemberUnmuteEvent)
+async def unmute_notice(app: GraiaApp, group: Group, member: Member = "target"):
+    await app.sendGroupMessage(group, MessageChain.create([
+        At(member.id),
+        Plain("开始学了")
+    ]))
 
 app.launch_blocking()
